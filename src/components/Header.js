f199e0b03cf58bcb01a1f920723641f005d437dc
sansrp358 <sanskrutirpaitwar@gmail.com>
2024-09-29 17:46:39 +0530

    Added background img and netfkix logo --sans

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
-import NetflixLogo from "../images/NetflixLogo.png";
 const Header = () => {
   return (
-    <div>
-      <img src={NetflixLogo} alt="NetflixLogo" className="w-8" />
+    <div className="absolute bg-gradient-to-b from-black">
+      <img
+        className="w-44 py-3 ml-28"
+        src="
+https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="NetflixLogo"
+      />
     </div>
   );
 };
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,12 @@ const Login = () => {
   return (
     <div>
       <Header />
-      Login Page Watch 3.09.10
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/bfc0fc46-24f6-4d70-85b3-7799315c01dd/web/IN-en-20240923-TRIFECTA-perspective_74e21c19-980e-45ef-bd6c-78c1a6ce9381_medium.jpg"
+          alt="Netflix-background-image"
+        />
+      </div>
     </div>
   );
 };
