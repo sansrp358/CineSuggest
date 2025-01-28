a0f189e9511254b48c351fe23abf2a4528511618
sansrp358 <sanskrutirpaitwar@gmail.com>
2025-01-28 21:51:47 +0530

    videotrailer adding

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,4 +1,14 @@
-const VideoBackground = () => {
+import { API_OPTIONS } from "../utils/constants";
+
+const VideoBackground = ({ movieId }) => {
+  //fetching movie trailer with the help of movie id
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/movie_id/videos?language=en-US",
+      API_OPTIONS
+    );
+    const response = 
+  };
   return <div>VideoBackground</div>;
 };
 export default VideoBackground;
